6d2c3ba722e8a450e5ca3a75f1f6d1c0b5638be2
pford <pford@potomacfusion.com>
2012-08-15 13:02:28 -0400

    Special Vars like $_

--- src/main/webapp/js/mjs/xp/SpecialVars.js
+++ src/main/webapp/js/mjs/xp/SpecialVars.js
@@ -14,31 +14,27 @@
             configurable: false,
             enumerable: false,
             set: function(callback){
-                var caller = arguments.callee.caller,
+                var caller = arguments ? arguments.callee.caller : null,
                     args = caller.arguments,
                     a, i, len, that = {};
+                if (!caller) return;
                 for (i = 0, len = args.length; i < len; ++i){
                     that[i] = callback(args[i], i)
                 }
+                //$.log("set").log(that);
                 caller.$_ = that;
             },
             get: function(){
-                return arguments.callee.caller.$_;
+                var caller = arguments.callee.caller,
+                    args = caller.$_;
+                if (!caller.$_) return Array.prototype.slice.call(caller.arguments);
+                return args;
             }
         }
     });
 
 
-    /**
-     *
-     * @param that
-     * @return {*}
-     */
-    Function.prototype.chain = function(that){
-        // When the new advised function executes, "this" should be the
-        // execution scope.  When chain() executes, "this" should be the
-        // original function.
-        return $.addAdvice(function(){ return that || this }).after(this);
-    }
+
+
 
 })(mjs);
