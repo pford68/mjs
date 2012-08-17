08760ca34ce08d7013778b3e8797d4179a204714
pford <paf1687@gmail.com>
2012-08-16 23:24:48 -0400

    Finished implementation

--- src/main/webapp/js/mjs/util/HashMap.js
+++ src/main/webapp/js/mjs/util/HashMap.js
@@ -1,4 +1,13 @@
 /**
+ * <p>A map that supports using any Object as a key, as long as that Object correctly implements a hash() method,
+ * similar to Java's hashCode().  The hash() method must return a unique value, though not necessarily an integer.
+ * Ideally the value should be dependent on properties in the Object.</p>
+ *
+ * <p>Inspired by the solution discussed at StackOverflow.  However, I do not use a doubly-linked list for fast
+ * iteration of keys and values, as that solution does.  I felt that was redundant since I already have a separate
+ * LinkedList implementation.</p>
+ *
+ * @see http://stackoverflow.com/questions/368280/javascript-hashmap-equivalent
  *
  * @author Philip Ford
  */
@@ -6,46 +15,56 @@
 
     $.require("mjs/core/oop");
     $.require("mjs/core/interfaces");
+    $.require("mjs/core/ObjectDecorator");
 
 
     $.util.HashMap = $.Class({
 
         _items: null,
+        _className: "mjs.util.HashMap",
 
-        initialize: function(list){
-            if (Array.isArray(list)) {
-                this.putAll(list);
-            }
+        initialize: function(map){
+            this._items = {};
+            if (map) this.putAll(map);
         },
 
         put: function(key, value){
-            this.items[Object.hash(key)] = value;
+            this._items[Object.hash(key)] = value;
         },
 
-        putAll: function(list){
-            list.forEach(function(item){
-
+        putAll: function(map){
+            var instance = this;
+            mjs.decorate(map).forEach(function(item, key){
+                instance.put(key, item);
             })
         },
 
         get: function(key){
-            return this.items[Object.hash(key)];
+            return this._items[Object.hash(key)];
         },
 
         keys: function(){
-            return Object.keys(this.items);
+            return Object.keys(this._items);
         },
 
         values: function(){
-            return Object.values(this.items);
+            var result = [];
+            this.forEach(function(item){
+                result.push(item);
+            });
+            return result;
         },
 
         size: function(){
-            return this.keys.length;
+            return Object.keys(this._items).length;
         },
 
         forEach: function(callback){
-            mjs.decorate(this.items).forEach(callback);
+            mjs.decorate(this._items).forEach(callback);
+        },
+
+        clear: function(){
+            this._items = {};
         }
 
     }).implement($.Iterable);
