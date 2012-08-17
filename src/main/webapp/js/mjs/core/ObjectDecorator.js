aeaf076b45994222d160b81807e038b3c35b874e
pford <paf1687@gmail.com>
2012-08-17 18:14:45 -0400

    Added doc comments and check for object equality in contains().

--- src/main/webapp/js/mjs/core/ObjectDecorator.js
+++ src/main/webapp/js/mjs/core/ObjectDecorator.js
@@ -33,6 +33,12 @@
 
     $.extend(ObjectDecorator.prototype, {
 
+        /**
+         * Mixes the properties of the arguments into the component.
+         *
+         * @param varargs
+         * @return {*}
+         */
         extend: function(varargs){
             function _extend(a, b){
                 for (var i in b){
@@ -49,7 +55,13 @@
             return this;   // For chaining
         },
 
-
+        /**
+         * Mixes the properties of the specified object into the component, but only properties that do not already
+         * exist in the component.  In other words, existing properties are not overridden in the component.
+         *
+         * @param that
+         * @return {*}
+         */
         augment: function(that){
             var i, cmp = this.component;
             for (i in that) {
@@ -60,7 +72,13 @@
         },
 
 
-
+        /**
+         * Adds the properties of the specified object to the component if and only if the component already
+         * has properties of the same name.
+         *
+         * @param that
+         * @return {*}
+         */
         override: function(that){
             var cmp = this.component;
             for (var i in that) {
@@ -72,7 +90,7 @@
 
 
         /**
-         *
+         * Checks whether all of the arguments are properties of the component.
          */
         has: function(varargs){
             var cmp = this.component;
@@ -113,6 +131,9 @@
 
 
         /**
+         * Returns an object of component key/value pairs that passed the filter.  The filter requires a callback
+         * function that takes the following parameters:  (1) the value of the current property, the name of the
+         * current property, and the component.  That function must return true/false.
          *
          * @param callback
          */
@@ -129,6 +150,7 @@
 
 
         /**
+         * Returns the number of key/value pairs in the object.
          *
          * @return {Number}
          */
@@ -138,6 +160,8 @@
 
 
         /**
+         * Returns an object containing the differences between the component and the specified object--differences
+         * being different properties, or properties with the same name but different values.
          *
          * @param that
          * @return {Object}
@@ -156,6 +180,8 @@
 
 
         /**
+         * Returns an object containing the properties that match (same name and value) between the component and
+         * the specified object.
          *
          * @param that
          * @return {*}
@@ -167,7 +193,11 @@
         },
 
 
-
+        /**
+         * Returns an array of the values in the component.
+         *
+         * @return {Array}
+         */
         values: function(){
             var result = [];
             if (this.component){
@@ -179,19 +209,41 @@
         },
 
 
-
+        /**
+         * Converts the component to JSON.  Little more than syntactic sugar since it
+         * merely calls JSON.stringify().
+         *
+         * @return {*}
+         */
         serialize: function(){
             return JSON.stringify(this.component);
         },
 
+
+        /**
+         * Returns "[object ObjectDecorator]," identifying the class.
+         *
+         * @return {String}
+         */
         toString: function(){
             return "[object ObjectDecorator]";
         },
 
 
+        /**
+         * Returns a "Specification" object that can be used to compare the spec for the component to
+         * the spec for another object.  Used for duck-typing.
+         */
         getSpec: function(){
             var spec = this.component;
             return Object.create({
+                /**
+                 * Returns true/false for whether the specified object has all of the properties in the component.
+                 * and whether the types of the corresponding properties match.
+                 *
+                 * @param that
+                 * @return {Boolean}
+                 */
                 like: function(that){
                     $.log("like start").log(that);
                     for (var i in spec){
@@ -204,6 +256,13 @@
                     }
                     return true;
                 },
+                /**
+                 * Returns true/false for whether the specs of the component and the specified object match exactly,
+                 * sharing all of the same properties, and whether the types of the corresponding properties match.
+                 *
+                 * @param that
+                 * @return {Boolean}
+                 */
                 equals: function(that){
                     for (var i in that){
                         if (that.hasOwnProperty(i) && that[i] !== null){
@@ -218,9 +277,26 @@
             });
         },
 
+
+
+        /**
+         * Returns true/false for whether the specified value is contained in the object.
+         *
+         * @param that
+         * @return {Boolean}
+         */
         contains: function(that){
-            for (var i in this.component){
-                if (this.component[i] == that) return true;
+            function compare(a, b){
+                if ($.isFunction(a.equals)){
+                    return a.equals(b)
+                }
+                return false;
+            }
+            var cmp = this.component;
+            for (var i in cmp){        // Intentionally not using hasOwnProperty for now.
+                if (cmp[i] == that || ($.isObject(cmp[i] && compare(cmp[i], that)))) {
+                    return true;
+                }
             }
             return false;
         }
