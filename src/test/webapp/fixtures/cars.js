9652efb4ed46640378f78a0fbb78dce7d018af45
pford <paf1687@gmail.com>
2012-08-18 14:19:11 -0400

    Added fixtures.  The deploy-js-test target copies them to mjs in the test app, and the fixtures are a module within mjs.

---
+++ src/test/webapp/fixtures/cars.js
@@ -0,0 +1,16 @@
+(function($){
+
+    $.fixtures.cars = [
+        { make: "Lamborghini", model: "Countach", year: 1989, color: "black" },
+        { make: "Lamborghini", model: "Aventador", year: 1989, color: "silver" },
+        { make: "Pontiac", model: "Solstice", year: 2006, color: 'silver' },
+        { make: "BMW", model: "328i", year: 2012, color: "black" },
+        { make: "BMW", model: "335i", year: 2011, color: "silver" },
+        { make: "Tesla", model: "Model S", year: 2012, color: "black" },
+        { make: "Tesla", model: "Roadster", year: 2012, color: "magenta" },
+        { make: "Porsche", model: "Boxster", year: 2012, color: 'silver' }
+    ];
+
+    //Object.freeze($.fixtures.cars);
+
+})(mjs);
