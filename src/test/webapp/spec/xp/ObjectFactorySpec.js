33f048452f90da62dabb6889a6d90a36bdd91445
pford <paf1687@gmail.com>
2012-08-26 02:15:01 -0400

    Added to core

--- src/test/webapp/spec/xp/ObjectFactorySpec.js
+++ src/test/webapp/spec/xp/ObjectFactorySpec.js
@@ -4,6 +4,10 @@ describe("mjs.getFactory()", function(){
 
     $.require("mjs/xp/ObjectFactory");
 
+    function getPrototype(that){
+        return that.__proto__ || that.getPrototypeOf();
+    }
+
     function assertObjectX(that){
         expect(typeof that.equals === 'function').toBeTruthy();
         expect(typeof that.compareTo === 'function').toBeTruthy();
@@ -19,7 +23,10 @@ describe("mjs.getFactory()", function(){
 
     var blueprint = {
         equals: function(that){ return this == that; },
-        compareTo: function(){}
+        compareTo: function(){},
+        toString: function(){
+            return "ObjectX";
+        }
     };
 
     $.log("Object.create").log(Object.create(blueprint));
@@ -31,11 +38,14 @@ describe("mjs.getFactory()", function(){
         marshal: function(){
 
         },
+        toString: function(){
+            return "Schema";
+        },
         fields: {}
     });
 
     var ObjectX = $.getFactory(blueprint);
-    var Schema = $.getFactory(schema, {
+    var Schema = ObjectX.extend(schema, {
         type: { value: "Schema", 'writable': false, enumerable: true }
     });
 
@@ -47,35 +57,78 @@ describe("mjs.getFactory()", function(){
 
 
     describe("The factory returned by getFactory()", function(){
-        it("should produce objects that have the same spec as the blueprint", function(){
-            var that = ObjectX.build();
-            $.log("objectx").log(that);
-            assertObjectX(that);
+
+        describe("build()", function(){
+            it("should produce objects that have the same spec as the blueprint", function(){
+                var that = ObjectX.build();
+                $.log("objectx").log(that);
+                assertObjectX(that);
+            });
+
+            it("changes to the blueprint should not affect the objects that it produces", function(){
+                var a = {
+                    equals: function(that){ return this == that; },
+                    compareTo: function(){}
+                };
+                var ObjectA = $.getFactory(a);
+                var first = ObjectA.build();
+                a.id = "Huh?";
+                delete a.compareTo;
+
+                var second = ObjectA.build();
+
+                assertObjectX(first);
+                assertObjectX(second);
+                expect(first.id).toBeUndefined();
+                expect(second.id).toBeUndefined();
+            });
+
+            it("should add properties and methods to the prototype of the object", function(){
+                var that = ObjectX.build();
+                var proto = getPrototype(that);
+                expect(proto.compareTo).toBeDefined();
+                expect(proto.equals).toBeDefined();
+            });
         });
 
-        it("changes to the blueprint should not affect the objects that it produces", function(){
-            var a = {
-                equals: function(that){ return this == that; },
-                compareTo: function(){}
-            };
-            var ObjectA = $.getFactory(a);
-            var first = ObjectA.build();
-            a.id = "Huh?";
-            delete a.compareTo;
 
-            var second = ObjectA.build();
-
-            assertObjectX(first);
-            assertObjectX(second);
-            expect(first.id).toBeUndefined();
-            expect(second.id).toBeUndefined();
-        });
 
         describe("extend()", function(){
             it("should produce objects that have the same spec as its new blueprint", function(){
                 var that = Schema.build();
                 $.log("schema").log(that);
                 assertSchema(that);
+                expect(that.toString()).toEqual("Schema");
+            });
+
+            it("should add properties and methods to the prototype of the object", function(){
+                var that = Schema.build();
+                var proto = getPrototype(that);
+                expect(proto.marshal).toBeDefined();
+                expect(proto.compareTo).toBeDefined();
+                expect(proto.equals).toBeDefined();
+                expect(proto.fields).toBeDefined();
+            });
+
+            it("should add not properties and methods to the prototype in the original factory", function(){
+                var that = ObjectX.build();
+                var proto = getPrototype(that);
+                expect(proto.marshal).toBeUndefined();
+                expect(proto.fields).toBeUndefined();
+            });
+
+            it("should override methods inherited from the original blueprint without affecting the original", function(){
+                var schema = Schema.build();
+                var ox = ObjectX.build();
+                expect(schema.toString()).toEqual("Schema");
+                expect(ox.toString()).toEqual("ObjectX");
+            });
+        });
+
+        describe("$super()", function(){
+            it("should retrieve the specified function of the parent factory's blueprint", function(){
+                var that = Schema.build();
+                expect(Schema.$super("toString", that).call()).toEqual("ObjectX");
             });
         });
     });
