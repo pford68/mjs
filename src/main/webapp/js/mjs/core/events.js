5b07c1af3bbdd248ccd074aca81d7bbf6445c879
pford <paf1687@gmail.com>
2012-08-29 23:29:48 -0400

    Changed the interface to addListener, though the old is still supported.

--- src/main/webapp/js/mjs/core/events.js
+++ src/main/webapp/js/mjs/core/events.js
@@ -6,7 +6,10 @@
     $.require("mjs/core/utils");
     $.require("mjs/core/html");
 
-    var $public, registry = {};
+    var $public, registry = {},
+        regex = {
+            on: /^on/
+        };
 
 
     $public = {
@@ -14,7 +17,7 @@
         /**
          *
          */
-        Listener:(function () {
+        Listener: (function () {
             /*
             Notes:
             - Holding a reference to the source (in this.src) is dangerous memory-wise, but merely holding the
@@ -35,30 +38,35 @@
 
         /**
          *
-         * @param args
+         * @param src
+         * @param evt
+         * @param cmd
+         * @param scope
+         * @return {*}
          */
-        addListener:function (args) {
-            //args = $.isObject(args, true) ? args : $.fromArguments(arguments, { 0: "src", 1: "evt", 2: "callback", 3: "scope"});
-            var src = $.isString(args.src) ? document.getElementById(args.src) : args.src,
-                evt = args.evt || args.event,
-                func = args.execute || args.callback || function (e) {
-                },
-                scope = args.scope,
-                listener, ls;
+        addListener: function(src, evt, cmd, scope) {
+            var args, listener, ls;
+            if (!evt){
+                args = src;
+                src = $.isString(args.src) ? document.getElementById(args.src) : args.src;
+                evt = args.evt || args.event;
+                cmd = args.execute || args.callback || function (e) {};
+                scope = args.scope;
+            }
 
             evt = evt.replace(regex.on, "");
-            func = scope ? $.proxy(scope, func) : func;
+            cmd = scope ? $.proxy(scope, cmd) : cmd;
 
             if (window.addEventListener) {
-                src.addEventListener(evt, func);
+                src.addEventListener(evt, cmd);
             } else if (window.attachEvent) {
                 evt = "on" + evt;
-                src.attachEvent(evt, func);
+                src.attachEvent(evt, cmd);
             } else {
                 evt = "on" + evt;
-                src[evt] = func;
+                src[evt] = cmd;
             }
-            listener = (args instanceof $public.Listener) ? args : new $public.Listener(src, evt, func);
+            listener = (args instanceof $public.Listener) ? args : new $public.Listener(src, evt, cmd);
             ls = src.listeners = src.listeners || {};
             ls[listener.id] = listener;
             registry[listener.id] = listener;
@@ -69,7 +77,7 @@
          *
          * @param listener
          */
-        removeListener:function (listener) {
+        removeListener: function(listener) {
             var src = listener.src;
             delete src.listeners[listener.id];
             delete registry[listener.id];
@@ -86,7 +94,7 @@
          *
          * @param src
          */
-        removeListeners:function (src) {
+        removeListeners: function(src) {
             var i, ls = src.listeners;
             for (i in ls) {
                 if (ls.hasOwnProperty(i)) $public.removeListener(ls[i]);
@@ -112,15 +120,10 @@
             } else {
                 src.fireEvent("on" + event.eventType, event);
             }
-        },
-
-
-        addDelegate: function(args){
-
         }
     };
 
-    $public.addListener({ src: window, evt: "unload", execute: function(e){
+    $public.addListener(window, "unload", function(e){
         var i, src;
         for (i in registry){
             if (registry.hasOwnProperty(i)){
@@ -131,7 +134,7 @@
             }
         }
         registry = null;
-    }});
+    });
 
     $.extend($public);
 
