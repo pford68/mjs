792f632332ecfbc0c56f655fb7ed516fb76e39c2
pford <paf1687@gmail.com>
2013-01-11 23:47:26 -0500

    Log properties for the example file.

---
+++ src/test/webapp/js/examples/logProperties.js
@@ -0,0 +1,12 @@
+/*
+ *
+ *
+ */
+mjs.config.logging = {
+    //logger: 'mjs/logging/AlertLogger',
+    pattern: "%d [%M] %p%-5l - %m%n",
+    //pattern: "[%M]%l............%m",
+    dateFormat: 'yyyy/MM/dd HH:mm:ss.SSS',
+    firstModule: "INFO",
+    PretendClass: "LOG"
+};
