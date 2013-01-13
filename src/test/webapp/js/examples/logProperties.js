249890da7d47524124b36be53a3b26a8abdce58a
pford <paf1687@gmail.com>
2013-01-12 23:54:19 -0500

    Updated for changes to LogFactory.

--- src/test/webapp/js/examples/logProperties.js
+++ src/test/webapp/js/examples/logProperties.js
@@ -1,12 +1,13 @@
 /*
- *
+ * An example log properties file.  This works like log4j properties, and Javascript ports of it,
+ * only simpler.
  *
  */
 mjs.config.logging = {
     //logger: 'mjs/logging/AlertLogger',
+    //pattern: "%d{yyyy-MM-dd HH:mm} [%M] %p%-5l - %m%n",
     pattern: "%d [%M] %p%-5l - %m%n",
-    //pattern: "[%M]%l............%m",
-    dateFormat: 'yyyy/MM/dd HH:mm:ss.SSS',
+    //pattern: "[%M]%l............%m%-5F",
     firstModule: "INFO",
     PretendClass: "LOG"
 };
